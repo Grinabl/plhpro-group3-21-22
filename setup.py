c55c3b225fd98e1637b8a1249d73e47bd472cb36
Grinabl <100235520+Grinabl@users.noreply.github.com>
2022-06-11 19:59:21 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+import py2exe
+
+#setup(console=['#####.py'])
+setup(windows=['#####.py'])
+
+
+#python setup.py py2exe
+#pyinstaller --onefile #####.py
+#pyinstaller --noconsole --onefile #####.py
